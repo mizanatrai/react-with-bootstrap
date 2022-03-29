443546de13d93b17bfb1c6fbbadc03a25292fac5
Mizanur Rahman <mizanbd.eee@gmail.com>
2022-03-30 03:04:02 +0600

    more work need to be done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import CardGroups from './components/CardGroup/cardGroup';
 import Card from './components/Card/Card';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <CardGroups></CardGroups>
+      <Button>My button</Button>
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import CardGroups from '../CardGroup/cardGroup';
 
 const Card = ({product}) => {
     const {name, id, price} = product;
     return (
         <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">Name {name}</h5>
-                <p className="card-text">Price: ${price}</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-            </div>
+            
         </div>
     );
 };
